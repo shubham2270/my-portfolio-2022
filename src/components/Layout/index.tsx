3fdafc1fc83a735e3b7d3a3c30d602c12455bb08
shubham2270 <shubham2270@gmail.com>
2022-01-23 21:29:42 +0530

    added path styling & current time to container

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,24 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 import styles from "./layout.module.scss";
-const { container, main, background } = styles;
+const { container, main, background, pathContainer, pathText, timeText } =
+  styles;
 
 const Layout = ({ children }) => {
   const videoRef = useRef(null);
+  const [time, setTime] = useState("");
+
+  function formatAMPM(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    const ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    const strTime = `${hours}:${minutes}:${seconds} ${ampm}`;
+    return strTime;
+  }
 
   useEffect(() => {
     if (videoRef && videoRef.current) {
@@ -12,13 +26,26 @@ const Layout = ({ children }) => {
     }
   }, []);
 
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      const currentTime = formatAMPM(new Date());
+      setTime(currentTime);
+    }, 1000);
+
+    return () => clearInterval(timeInterval);
+  });
+
   return (
     <div className={main}>
       <video autoPlay muted loop className={background} ref={videoRef}>
         <source src='/assets/background.mp4' type='video/mp4'></source>
       </video>
-      {/* <main className={background}></main> */}
-      <div className={container}></div>
+      <div className={container}>
+        <div className={pathContainer}>
+          <span className={pathText}>~/Home</span>
+          <span className={timeText}>{time}</span>
+        </div>
+      </div>
     </div>
   );
 };
