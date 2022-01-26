8ea071b87323fcfceb9e0caf4e56a3475e4264c9
shubham2270 <shubham2270@gmail.com>
2022-01-26 16:38:46 +0530

    made nav responsive & fix flashing background on page change

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,32 +1,22 @@
-import React, { useRef, useEffect, useState } from "react";
-import Navbar from "../Navbar";
+import React, { useRef, useEffect, useState } from 'react';
+import Navbar from '../Navbar';
 
-import styles from "./layout.module.scss";
-const {
-  container,
-  main,
-  background,
-  pathContainer,
-  pathText,
-  timeText,
-  navContainer,
-} = styles;
+import styles from './layout.module.scss';
+const { container, main, background, pathContainer, pathText, timeText, navContainer } = styles;
 
 const Layout = ({ children }) => {
   const videoRef = useRef(null);
-  const [time, setTime] = useState("");
+  const [time, setTime] = useState('');
 
   const formatAMPM = (date: Date) => {
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
-    const ampm = hours >= 12 ? "pm" : "am";
+    const ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? 0 + minutes : minutes;
-    const strTime = `${hours}:${minutes}:${
-      seconds < 10 ? `0${seconds}` : seconds
-    } ${ampm}`;
+    const strTime = `${hours}:${minutes}:${seconds < 10 ? `0${seconds}` : seconds} ${ampm}`;
     return strTime;
   };
 
@@ -48,7 +38,7 @@ const Layout = ({ children }) => {
   return (
     <div className={main}>
       <video autoPlay muted loop className={background} ref={videoRef}>
-        <source src='/assets/background.mp4' type='video/mp4'></source>
+        <source src="/assets/background.mp4" type="video/mp4"></source>
       </video>
       <div className={container}>
         <div className={pathContainer}>
@@ -58,6 +48,7 @@ const Layout = ({ children }) => {
         <div className={navContainer}>
           <Navbar />
         </div>
+        {children}
       </div>
     </div>
   );
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Link from 'next/link';
+
+import styles from './navbar.module.scss';
+const { main, navItem } = styles;
+
+const Navbar = () => {
+  const navItems = [
+    {
+      name: 'Home',
+      href: '/',
+    },
+    {
+      name: 'About',
+      href: '/about',
+    },
+    {
+      name: 'Skills',
+      href: '/skills',
+    },
+    {
+      name: 'Projects',
+      href: '/projects',
+    },
+    {
+      name: 'Connect',
+      href: '/connect',
+    },
+  ];
+
+  return (
+    <main className={main}>
+      {navItems.map(item => {
+        const { name, href } = item;
+        return (
+          <Link href={href} key={name}>
+            <a className={navItem}>{name}</a>
+          </Link>
+        );
+      })}
+    </main>
+  );
+};
+
+export default Navbar;
